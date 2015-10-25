21cebc724ee0c98c0f2d9777fe20283ef9d2de22
carolmlb <carolmlb@icloud.com>
2015-10-25 16:53:03 -0400

    Commit changes made to cacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,12 @@
-## Put comments here that give an overall description of what your
+## Functions: makeCacheMatrix and cacheSolve
+##
+## Programmer: Carolmlb (Carol Roffer)
+## Source Programmer: rdpeng
+## Course: R Programming (October 2015)
+##
+## This file contains two functions; makeCacheMatrix and cacheSolve. The pair
+## of functions
+Put comments here that give an overall description of what your
 ## functions do
 
 ## Write a short comment describing this function
